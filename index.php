63c4edf4680cc44a2335b7b0bb4e54731505adda
siva <sivakumaran.e@gmail.com>
2013-06-23 03:38:47 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+
+ <?php
+
+      include_once("simple_html_dom.php");
+
+      $url = "http://www.interviewstreet.com";
+
+     $html = file_get_html($url);
+
+      foreach($html->find("a") as $link) {
+        echo $link->href."<br/>";
+      }
+
+ ?>
