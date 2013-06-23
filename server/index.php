8e6381c89c78d79d788576304b2f3bd949cbc57a
siva <sivakumaran.e@gmail.com>
2013-06-23 06:37:56 -0700

    backbone implementation

--- server/index.php
+++ server/index.php
@@ -1,14 +1,28 @@
 
- <?php
-      set_time_limit(0);
-      include_once("simple_html_dom.php");
+<?php
+ 
+	require 'Slim/Slim.php';
+	use Slim\Slim;
+	\Slim\Slim::registerAutoloader();
+	include_once("simple_html_dom.php");
+	
+	$app = new Slim();
+	
+	
+	/*Get all links on page*/
+	$app->get('/links',function($url) use($app) {
+		$html = file_get_html($url);
 
-      $url = "http://www.interviewstreet.com";
+		foreach($html->find("a") as $link) {
+		echo $link->href."<br/>";
+				
+		
+	});
+	
+	
+	
 
-     $html = file_get_html($url);
-
-      foreach($html->find("a") as $link) {
-        echo $link->href."<br/>";
-      }
+	
+	$app->run();
 
  ?>
